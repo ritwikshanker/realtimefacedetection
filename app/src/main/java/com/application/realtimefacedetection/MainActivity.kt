a146b97258ad1689056a926b8e0beed7c28c0539
Ritwik <ritwikshanker@yahoo.in>
2021-11-09 16:59:34 +0530

    Added code for face detection

--- app/src/main/java/com/application/realtimefacedetection/MainActivity.kt
+++ app/src/main/java/com/application/realtimefacedetection/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.application.realtimefacedetection
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceContour
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetector
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.google.mlkit.vision.face.FaceLandmark
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ImageAnalysis.Analyzer {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupFaceDetection()
+    }
+
+    private lateinit var detector: FaceDetector
+    private fun setupFaceDetection() {
+        val realTimeOpts = FaceDetectorOptions.Builder()
+            .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+            .build()
+        detector = FaceDetection.getClient(realTimeOpts)
+    }
+
+    @androidx.camera.core.ExperimentalGetImage
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            val result = detector.process(image)
+                .addOnSuccessListener { faces ->
+
+                    for (face in faces) {
+                        val bounds = face.boundingBox
+                        val rotY = face.headEulerAngleY // Head is rotated to the right rotY degrees
+                        val rotZ = face.headEulerAngleZ // Head is tilted sideways rotZ degrees
+
+                        // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                        // nose available):
+                        val leftEar = face.getLandmark(FaceLandmark.LEFT_EAR)
+                        leftEar?.let {
+                            val leftEarPos = leftEar.position
+                        }
+
+                        // If contour detection was enabled:
+                        val leftEyeContour = face.getContour(FaceContour.LEFT_EYE)?.points
+                        val upperLipBottomContour =
+                            face.getContour(FaceContour.UPPER_LIP_BOTTOM)?.points
+
+                        // If classification was enabled:
+                        if (face.smilingProbability != null) {
+                            val smileProb = face.smilingProbability
+                        }
+                        if (face.rightEyeOpenProbability != null) {
+                            val rightEyeOpenProb = face.rightEyeOpenProbability
+                        }
+
+                        // If face tracking was enabled:
+                        if (face.trackingId != null) {
+                            val id = face.trackingId
+                        }
+                    }
+                }
+                .addOnFailureListener { e ->
+                    // Task failed with an exception
+                    // ...
+                }
+        }
     }
 }
