b437d30bd7c70a7376e850d02a4735235dd6d1af
Ritwik <ritwikshanker@yahoo.in>
2021-11-25 17:30:11 +0530

    Added check if developer option is enabled or not

--- app/src/main/java/com/application/realtimefacedetection/proctoring/ProctoringActivity.kt
+++ app/src/main/java/com/application/realtimefacedetection/proctoring/ProctoringActivity.kt
@@ -3,6 +3,7 @@ package com.application.realtimefacedetection.proctoring
 import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -16,17 +17,36 @@ class ProctoringActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding =
             DataBindingUtil.setContentView(this, R.layout.activity_proctoring)
+
+        checkDeveloperOption()
+    }
+
+    private fun checkDeveloperOption() {
+        val adb: Int = Settings.Secure.getInt(
+            this.contentResolver,
+            Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0
+        )
+        if (adb == 1) {
+            Toast.makeText(this, "Developer Option is on", Toast.LENGTH_SHORT).show()
+        } else {
+
+            Toast.makeText(this, "Normie User", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onMultiWindowModeChanged(isInMultiWindowMode: Boolean, newConfig: Configuration?) {
-        Toast.makeText(this, "Don't you dare try to cheat - onMultiWindowModeChanged", Toast.LENGTH_LONG).show()
+        Toast.makeText(
+            this,
+            "Don't you dare try to cheat - onMultiWindowModeChanged",
+            Toast.LENGTH_LONG
+        ).show()
         super.onMultiWindowModeChanged(isInMultiWindowMode, newConfig)
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onResume() {
         super.onResume()
-        if (isInMultiWindowMode){
+        if (isInMultiWindowMode) {
             Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
         }
     }
